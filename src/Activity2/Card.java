bc6bba3c1441ea659d8e1a5289a6a2311702d26c
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-02-26 08:22:11 -0500

    Update Card.java

--- src/Activity2/Card.java
+++ src/Activity2/Card.java
@@ -1,7 +1,48 @@
-package Activity2;
-
 /**
  * Created by Teacher on 1/7/2019.
  */
-public class Card {
+public class Card
+{
+    private String suit;
+    private String rank;
+    private int point;
+
+    public Card(String suit, String rank, int point) {
+        this.suit = suit;
+        this.rank = rank;
+        this.point = point;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", rank='" + rank + '\'' +
+                ", point=" + point +
+                '}';
+    }
 }
